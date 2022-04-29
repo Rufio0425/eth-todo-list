e4069c6a77392166029e23d4b9959a87fa52ff98
Sean Aguirre <seanpeter.aguirre@gmail.com>
2022-04-28 23:48:03 -0700

    added createTask()

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -3,4 +3,21 @@ pragma solidity ^0.5.0;
 // This is the ACTUAL smart contract, hence the "contract" identifier
 contract TodoList {
     uint public taskCount = 0;
+
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint => Task) public tasks;
+
+    constructor() public {
+        createTask("Do 10 push ups!");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount ++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
 }
